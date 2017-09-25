6b8332ff07dfa631167d34988e0333ce5be6412a
Vishesh Yadav <vishesh3y@gmail.com>
2017-09-24 22:27:46 -0700

    WIP

--- racketscript-extras/racketscript/experimental/generator.rkt
+++ racketscript-extras/racketscript/experimental/generator.rkt
@@ -15,7 +15,88 @@
 (define (yield v)
   (error 'yield "Should be compiled to something else!"))
 
-(define (lift-bindings stx)
+#;(define (lift-bindings stx)
+  (define lexical-bindings (make-parameter #f))
+
+  (define (save-bindings! b*)
+    (stx-map (λ (i)
+               (dict-set! (lexical-bindings) i #t))
+             b*))
+
+  (define default-value-stx #'(#%plain-app void))
+
+  (define (lift stx)
+    (syntax-parse stx
+      #:literal-sets ((kernel-literals))
+      [(#%plain-lambda formals body ...+)
+       (parameterize ([lexical-bindings (make-free-id-table)])
+         (with-syntax ([(lifted-body ...) (stx-map lift #'(body ...))]
+                       [(collected-bindings ...) (map (λ (b)
+                                                        #`((#,b) #,default-value-stx))
+                                                      (dict-keys (lexical-bindings)))])
+           (with-syntax ([new-body  (syntax-property #'(let-values (collected-bindings ...)
+                                                         lifted-body ...)
+                                                     'racketscript-generator-lifted-let-bindings
+                                                     #t)])
+             #`(#%plain-lambda formals new-body))))]
+      [(let-values ([(id ...) bb] ...) body ...+)
+       #:with (lifted-body ...) (stx-map lift #'(body ...))
+       #:with (lifted-bb ...) (stx-map lift #'(bb ...))
+       #:with ((fresh-id ...) ...) (map (λ (ids)
+                                          (generate-temporaries (syntax-e ids)))
+                                        (syntax-e #'((id ...) ...)))
+       #:with (set!-bindings ...) (stx-map (syntax-parser
+                                             [((id) bb) (lift #'(set! id bb))]
+                                             #;[((id ...) bb)
+                                                #:with (fresh-id ...) (generate-temporaries (syntax-e #'(id ...)))
+                                                #'(let-values ([(fresh-id ...) bb])
+                                                    (set! id fresh-id) ...)])
+                                           #'([(id ...) lifted-bb] ...))
+
+       (save-bindings! #'(id ... ...))
+       #`(let-values () set!-bindings ... lifted-body ...)]
+      [(letrec-values ([(id ...) bb] ...) body ...+)
+       (error 'normalize "letrec unimplemented")]
+      [(with-continuation-mark key val result)
+       (error 'normalize "w-c-m unimplemented")]
+      [(if pred t-branch f-branch)
+       #`(if #,(lift #'pred)
+             #,(lift #'t-branch)
+             #,(lift #'f-branch))]
+      [(case-lambda . clauses)
+       (error 'normalize "case-lambda unimplemented")]
+      [(#%plain-app lam . args)
+       #:with lifted-lam (lift #'lam)
+       #:with lifted-args (stx-map lift #'args)
+       #`(#%plain-app lifted-lam . lifted-args)]
+      [(begin e ...+)
+       ;#:with (lifted-e ...) (stx-map lift #'(e ...))
+       #;#`(begin lifted-e ...)
+       (error 'lift-bindings "begin should be expanted to let-values")]
+      #;[(begin0 e0 e ...)
+       #:with lifted-e0 (lift #'e0)
+       #:with (lifted-e ...) (stx-map lift #'(e ...))
+       #`(begin0 lifted-e0 lifted-e ...)]
+      [(set! id expr)
+       #:with lifted-expr (lift #'expr)
+       #:with (temp) (generate-temporaries (list #'id))
+       #`(set! id lifted-expr)
+       #;(cond
+           [(stx-terminal? #'lifted-expr) #`(set! id lifted-expr)]
+           [else
+            (save-bindings! #'(temp))
+            #`(let-values ()
+                (set! temp lifted-expr)
+                (set! id temp))])]
+      [(#%expression expr)
+       #:with lifted-expr (lift #'expr)
+       #`(#%expression lifted-expr)]
+      [v #'v]))
+
+  (parameterize ([lexical-bindings (make-free-id-table)])
+    (lift stx)))
+
+(define (lift-bindings stx k)
   (define lexical-bindings (make-parameter #f))
 
   (define (save-bindings! b*)
@@ -128,7 +209,7 @@
     (pretty-print (syntax->datum prog))
     (pretty-print (syntax->datum prog2))
     #;(pretty-print (syntax->datum  (normalize prog identity)))
-    (pretty-print (syntax->datum  (lift-bindings (lift-bindings prog2))))
+    #;(pretty-print (syntax->datum  (lift-bindings (lift-bindings prog2))))
     #;(pretty-print (syntax->datum (expand (lift-bindings (lift-bindings (normalize prog identity))))))
     #;(pretty-print (syntax->datum prog2))
     #;(pretty-print (syntax->datum (lift-bindings prog2)))
